1988289a9c788267ce45ab7e17d5548e0feb5f8c
DamienRoussos <61167472+DamienRoussos@users.noreply.github.com>
2024-09-13 14:28:10 +0200

    changed searchbar from using value to placeholder

--- app/src/main/java/com/example/jetapp/ui/restaurantsearch/RestaurantSearch.kt
+++ app/src/main/java/com/example/jetapp/ui/restaurantsearch/RestaurantSearch.kt
@@ -1,21 +1,41 @@
 package com.example.jetapp.ui.restaurantsearch
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
 fun RestaurantSearch(
     userPostCode: String,
-    onPostCodeEnter: (String) -> Unit
+    onPostCodeEnter: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+
+    var postCodeInput by remember { mutableStateOf("") }
+
     TextField(
-        value = userPostCode,
+        value = postCodeInput,
         onValueChange = {
-            updatedPostCode ->
-                onPostCodeEnter(updatedPostCode)
+            updatedInput ->
+                postCodeInput = updatedInput
+                onPostCodeEnter(updatedInput)
         },
-        modifier = Modifier.fillMaxWidth()
+        placeholder = {
+            if(postCodeInput.isEmpty()) {
+                Text(text = userPostCode)
+            }
+        },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Text),
+        modifier = modifier.fillMaxWidth()
     )
 }
